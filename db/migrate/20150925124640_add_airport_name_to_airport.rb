ae23aeed053bcd79796b8499b8a25f33e3d7f27a
sppno1 <shreyasparikh2@gmail.com>
2015-10-02 17:53:21 -0400

    removed migration files

--- db/migrate/20150925124640_add_airport_name_to_airport.rb
+++
@@ -1,5 +0,0 @@
-class AddAirportNameToAirport < ActiveRecord::Migration
-  def change
-    add_column :airports, :airport_name, :string
-  end
-end
--- db/migrate/20150925125002_add_airline_name_to_airline.rb
+++
@@ -1,5 +0,0 @@
-class AddAirlineNameToAirline < ActiveRecord::Migration
-  def change
-    add_column :airlines, :airline_name, :string
-  end
-end
